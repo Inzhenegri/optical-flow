6dbd9b0cece2356ef28fdce4ea25c09ef6e6ae56
Arseniyyy <arskosh05@mail.ru>
2021-07-09 14:26:37 +0700

    Add Farneback optical flow algorithm

--- optical_flow.py
+++ optical_flow.py
@@ -11,8 +11,8 @@ options = {
 }
 
 client = NetGear(
-    # address='192.168.11.145', # school network
-    address='192.168.11.137', # home network
+    address='192.168.11.145', # school network
+    # address='192.168.11.137', # home network
     port='5454',
     pattern=2,
     receive_mode=True,
@@ -23,7 +23,7 @@ client = NetGear(
 
 feature_params=  dict(
     maxCorners=100,
-    qualityLevel=0.3,
+    qualityLevel=0.03,
     minDistance=7,
     blockSize=7
 )
---
+++ optical_flow_dense.py
@@ -0,0 +1,71 @@
+from vidgear.gears import NetGear
+import cv2
+import numpy as np
+
+
+options = {
+    'flag': 0,
+    'copy': False,
+    'track': False
+}
+
+client = NetGear(
+    address='192.168.11.145', # school network
+    # address='192.168.11.137', # home network
+    port='5454',
+    pattern=2,
+    receive_mode=True,
+    logging=True,
+    protocol='tcp',
+    **options
+)
+
+frame1 = client.recv()
+prvs = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+
+hsv = np.zeros_like(a=frame1)
+hsv[..., 1] = 255
+
+
+def start():
+    global prvs
+    while True:
+        frame2 = client.recv()
+
+        if frame2 is None:
+            break
+
+        next_frame = cv2.cvtColor(src=frame2, code=cv2.COLOR_BGR2GRAY)
+        flow = cv2.calcOpticalFlowFarneback(
+            prev=prvs,
+            next=next_frame,
+            flow=None,
+            pyr_scale=0.5,
+            levels=3,
+            winsize=15,
+            iterations=3,
+            poly_n=5,
+            poly_sigma=1.2,
+            flags=0
+        )
+
+        mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+        hsv[..., 0] = ang * 180 / np.pi / 2
+        hsv[..., 2] = cv2.normalize(src=mag, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+
+        bgr = cv2.cvtColor(src=hsv, code=cv2.COLOR_HSV2BGR)
+
+        cv2.imshow('frame2', bgr)
+
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+
+        prvs = next_frame
+        break
+
+
+start()
+
+cv2.destroyAllWindows()
+client.close()
